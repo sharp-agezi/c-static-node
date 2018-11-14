b70fbb2e7de4d3d556d9b25c3fab2243c0dee1a5
sharp-agezi <1150211686@qq.com>
2018-11-14 19:24:29 +0800

    c-static-node

---
+++ app.js
@@ -0,0 +1,34 @@
+'use strict';
+const Koa = require('koa');
+const app = new Koa();
+
+/**
+ *当请求开始时首先请求流通过 x-response-time 和 logging 中间件,
+  然后继续移交控制给 response 中间件。
+  当一个中间件调用 next() 则该函数暂停并将控制传递给定义的下一个中间件。
+  当在下游没有更多的中间件执行后，堆栈将展开并且每个中间件恢复执行其上游行为。
+ */
+
+//logging
+app.use(async (ctx, next)=>{
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log(`${ctx} - ${rt}`);
+});
+
+//x-response-time
+app.use(async (ctx, next)=>{
+    const start = Date.now();
+    await next();
+    const ms = Date.now() -start;
+    ctx.set('X-Response-Time', `${ms}ms`)
+});
+
+//response
+app.use(async ctx=>{
+    ctx.body = 'Koa'
+});
+
+app.listen(3000, ()=>{
+    console.log('listenning on 3000 ---http://localhost:3000')
+});
---
+++ src/util/fs.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// 处理静态模板合并的
+const fs = require('fs')

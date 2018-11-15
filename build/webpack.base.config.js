2050d517bae638d05dd84546f1c718b3ed4e11db
sharp-agezi <1150211686@qq.com>
2018-11-16 01:38:23 +0800

    webpack配置

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -10,5 +10,11 @@ module.exports = {
     },
     plugins:[
         new uglify() //插件是多个插件，所以是数组
-    ]
+    ],
+    devServer:{ //webpack-dev-server 热服务配置
+        contentBase:path.resolve(__dirname,'dist'),
+        host:'172.16.64.59',
+        compress:true,
+        port:8080
+    } //  配置webpack服务
 };

394a138e6efb178f4978ae13dd4b5d614f2b86d9
sharp-agezi <1150211686@qq.com>
2018-11-15 15:01:48 +0800

    编译支持html文件

---
+++ build/webpack.base.config.js
@@ -0,0 +1,10 @@
+//webpack.base.config
+const path = require('path');
+
+module.exports = {
+    entry : './src/js/entry.js',//入口文件
+    output : {//输出文件
+        filename : 'index.js',//输出文件名
+        path : __dirname + '/out'//输出文件路径
+    },
+};

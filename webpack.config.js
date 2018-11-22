cf5267bae038c4e5bb8588ff43c63db665db592f
sharp-agezi <1150211686@qq.com>
2018-11-22 10:28:27 +0800

    node koa 配置

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,20 @@ const uglify = require('uglifyjs-webpack-plugin'); //配置js的压缩和打包
 const ExtractTextPlugin = require('extract-text-webpack-plugin'); //配置css打包
 
 module.exports = {
-    entry : './src/js/entry.js',//入口文件
+    entry : './src/main.js',//入口文件
     output : {//输出文件
         filename : 'index.js',//输出文件名
         path : __dirname + 'dist'//输出文件路径
     },
     plugins:[
         // new uglify(), //插件是多个插件，所以是数组
-        new ExtractTextPlugin('/css/index.css')
+        new ExtractTextPlugin({ //处理css 插件
+            filename:"css/index.css"
+        })
     ],
+    resolve: {
+        extensions: ['.js'],
+    },
     module:{
         rules:[
             {
